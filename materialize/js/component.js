3e6cbd7e88c74ffd3c249f7258c49eae1175e74b
maldadcoorp <brandoncoorp@gmail.com>
2022-08-22 10:02:28 -0400

    contact
    contact

--- materialize/js/component.js
+++ materialize/js/component.js
@@ -4,4 +4,26 @@ $(document).ready(function(){
       $( "#mensaje" ).click(function() {
             alertify.success('Mensaje Enviado Exitosamente');
           });
+
+      $('#enviar').on('click',enviarMensaje);
    });
+
+   
+   async function enviarMensaje(){
+      var formu = document.getElementById("formu");
+      var boton2 = document.getElementById("enviar");
+      boton2.disabled = true;
+      const form = new FormData(formu);
+      const response = await fetch("https://formspree.io/f/xjvzkbvb", {
+            method: formu.method,
+            body: form,
+            headers: {
+              'Accept': 'application/json'
+            }
+          })
+          if(response.ok) {
+            formu.reset()
+            alertify.success('Mensaje Enviado Exitosamentee');
+          }
+     
+   }
